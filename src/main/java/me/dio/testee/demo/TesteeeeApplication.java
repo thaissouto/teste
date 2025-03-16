6c0306aaab836528556290598ef332647fe18f42
Thaís Souto <thaissouto@MacBook-Air-Thais-Souto.local>
2025-03-15 21:49:54 -0300

    Initial commit

---
+++ src/main/java/me/dio/testee/demo/TesteeeeApplication.java
@@ -0,0 +1,13 @@
+package me.dio.testee.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteeeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/testee/demo/TesteeeeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.testee.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteeeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
